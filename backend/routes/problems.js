c1882647ebbc6e4a8a25979b3354bf97bd810aee
kdduan <shizhuo.duan@stern.nyu.edu>
2020-07-08 22:20:17 -0400

    working version of bar chart

--- backend/routes/problems.js
+++ backend/routes/problems.js
@@ -3,59 +3,78 @@ let Problem = require("../models/problem.model");
 
 // handles incoming HTTP GET requests on the /problems/ URL
 router.route("/user/:id").get((req, res) => {
-  // get a list of all the problems from the database
-  Problem.find({ user_id: req.params.id })
-    .then((problems) => res.json(problems))
-    .catch((err) => res.status(400).json("Error: " + err));
+    // get a list of all the problems from the database
+    Problem.find({ user_id: req.params.id })
+        .then((problems) => res.json(problems))
+        .catch((err) => res.status(400).json("Error: " + err));
 });
 
 //  handles incoming HTTP POST requests on the /problems/add/ URL path
 router.route("/add").post((req, res) => {
-  // extract problem data from request body (default = String)
-  const number = Number(req.body.number);
-  const title = req.body.title;
-  const difficulty = req.body.difficulty;
-  const date = Date.parse(req.body.date);
-  const topics = req.body.topics;
-  const notes = req.body.notes;
-  const user_id = req.body.user_id;
+    // extract problem data from request body (default = String)
+    const number = Number(req.body.number);
+    const title = req.body.title;
+    const difficulty = req.body.difficulty;
+    const date = Date.parse(req.body.date);
+    const topics = req.body.topics;
+    const notes = req.body.notes;
+    const user_id = req.body.user_id;
 
-  const newProblem = new Problem({
-    number,
-    title,
-    difficulty,
-    date,
-    topics,
-    notes,
-    user_id,
-  });
+    const newProblem = new Problem({
+        number,
+        title,
+        difficulty,
+        date,
+        topics,
+        notes,
+        user_id,
+    });
 
-  newProblem
-    .save()
-    .then(() => res.json("Problem added!"))
-    .catch((err) => res.status(400).json("Error: " + err));
+    newProblem
+        .save()
+        .then(() => res.json("Problem added!"))
+        .catch((err) => res.status(400).json("Error: " + err));
 });
 
 // returns a problem item given an id
 router.route("/:id").get((req, res) => {
-  Problem.findById(req.params.id)
-    .then((problem) => res.json(problem))
-    .catch((err) => res.status(400).json("Error: " + err));
+    Problem.findById(req.params.id)
+        .then((problem) => res.json(problem))
+        .catch((err) => res.status(400).json("Error: " + err));
 });
 
 // deletes a problem item given an id
 router.route("/:id").delete((req, res) => {
-  Problem.findByIdAndDelete(req.params.id)
-    .then(() => res.json("Problem deleted."))
-    .catch((err) => res.status(400).json("Error: " + err));
+    Problem.findByIdAndDelete(req.params.id)
+        .then(() => res.json("Problem deleted."))
+        .catch((err) => res.status(400).json("Error: " + err));
 });
 
 // gets problems by difficulty
-router.route("/analytics/:uid/:diff").get((req, res) => {
-  Problem.find({ difficulty: req.params.diff, user_id: req.params.uid })
-    .then((problems) => res.json(problems))
-    .catch((err) => res.status(400).json("Error: " + err));
+router.route("/analytics/:uid").get((req, res) => {
+    Problem.find({ user_id: req.params.uid })
+        .then((problems) => {
+            const labels = ["Easy", "Medium", "Hard"]
+            const counts = [0, 0, 0]
+            problems.forEach((problem) => {
+                if (problem.difficulty === 'Easy') {
+                    counts[0]++;
+                } else if (problem.difficulty === 'Medium') {
+                    counts[1]++;
+                } else {
+                    counts[2]++;
+                }
+            })
+            const chartData = {
+                labels: labels,
+                title: "Problems Solved",
+                data: counts,
+            }
+            res.json(chartData);
+        })
+        .catch((err) => res.status(400).json("Error: " + err));
 });
+
 
 // Problem.find({ user_id: req.params.id })
 //     .then((problems) => res.json(problems))
--- src/components/Analytics.js
+++ src/components/Analytics.js
@@ -1,30 +1,49 @@
 import React, { useState, useContext } from "react";
+import { Bar } from 'react-chartjs-2';
 import axios from "axios";
 import { AuthContext } from "../auth.js";
 
 const Analytics = () => {
-    const [diff, setdiff] = useState("");
-    const [count, setCount] = useState("");
-
+    const [barChart, setBarChart] = useState({});
     const { currentUser } = useContext(AuthContext);
 
-    const handleClick = (e) => {
-        const buttonDiff = e.currentTarget.textContent;
-        setdiff(buttonDiff);
+    const barChartClick = (e) => {
         axios
-            .get(`http://localhost:5000/problems/analytics/${currentUser.uid}/${buttonDiff}`)
-                .then((res) => setCount(res.data.length));
+            .get(`http://localhost:5000/problems/analytics/${currentUser.uid}`)
+                .then((res) => {
+                    console.log(res.data)
+                    const chartData = {
+                        labels: res.data.labels,
+                        datasets: [
+                            {
+                                label: res.data.title,
+                                data: res.data.data
+                            }
+                        ]
+                    }
+                    console.log(chartData);
+                    setBarChart(chartData);
+                });
             };
 
     return (
         <>
         <h4>Analytics Page</h4>
-        <p>
-        {count} {diff}
-        </p>
-        <button onClick={(e) => handleClick(e)}>Easy</button>
-        <button onClick={(e) => handleClick(e)}>Medium</button>
-        <button onClick={(e) => handleClick(e)}>Hard</button>
+        <button onClick={(e) => barChartClick(e)}>Bar Chart</button>
+        <Bar
+            data={barChart}
+        options={{
+            title: {
+                display: true,
+                text: "Problems Solved",
+                fontSize:20
+            },
+            legend: {
+                display: true,
+                position: 'right'
+            }
+        }}
+        />
         </>
     );
 };
